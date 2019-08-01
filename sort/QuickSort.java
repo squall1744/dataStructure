8400298bea15d08a70c7bd8bd4abfdea0d2f8e42
squall1744 <squall1744@163.com>
2019-08-01 17:31:29 +0800

    Update QuickSort.java

--- sort/QuickSort.java
+++ sort/QuickSort.java
@@ -1,54 +1,46 @@
 import java.util.*;
 
-// 优化的Merge Sort算法
-// 在课程中, 主要向大家介绍了归并排序的两个优化点
-// 关于归并排序的更多优化, 请参考本章节后续的补充内容
-public class MergeSortO{
+public class QuickSort3Ways {
 
     // 我们的算法类不允许产生任何实例
-    private MergeSortO(){}
+    private QuickSort3Ways(){}
 
-    // 将arr[l...mid]和arr[mid+1...r]两部分进行归并
-    private static void merge(Comparable[] arr, int l, int mid, int r) {
+    // 递归使用快速排序,对arr[l...r]的范围进行排序
+    private static void sort(Comparable[] arr, int l, int r){
 
-        Comparable[] aux = Arrays.copyOfRange(arr, l, r+1);
-
-        // 初始化，i指向左半部分的起始索引位置l；j指向右半部分起始索引位置mid+1
-        int i = l, j = mid+1;
-        for( int k = l ; k <= r; k ++ ){
-
-            if( i > mid ){  // 如果左半部分元素已经全部处理完毕
-                arr[k] = aux[j-l]; j ++;
-            }
-            else if( j > r ){   // 如果右半部分元素已经全部处理完毕
-                arr[k] = aux[i-l]; i ++;
-            }
-            else if( aux[i-l].compareTo(aux[j-l]) < 0 ){  // 左半部分所指元素 < 右半部分所指元素
-                arr[k] = aux[i-l]; i ++;
-            }
-            else{  // 左半部分所指元素 >= 右半部分所指元素
-                arr[k] = aux[j-l]; j ++;
-            }
-        }
-    }
-
-    // 递归使用归并排序,对arr[l...r]的范围进行排序
-    private static void sort(Comparable[] arr, int l, int r) {
-
-        // 优化2: 对于小规模数组, 使用插入排序
+        // 对于小规模数组, 使用插入排序
         if( r - l <= 15 ){
             InsertionSort.sort(arr, l, r);
             return;
         }
 
-        int mid = (l+r)/2;
-        sort(arr, l, mid);
-        sort(arr, mid + 1, r);
+        // 随机在arr[l...r]的范围中, 选择一个数值作为标定点pivot
+        swap( arr, l, (int)(Math.random()*(r-l+1)) + l );
 
-        // 优化1: 对于arr[mid] <= arr[mid+1]的情况,不进行merge
-        // 对于近乎有序的数组非常有效,但是对于一般情况,有一定的性能损失
-        if( arr[mid].compareTo(arr[mid+1]) > 0 )
-            merge(arr, l, mid, r);
+        Comparable v = arr[l];
+
+        int lt = l;     // arr[l+1...lt] < v
+        int gt = r + 1; // arr[gt...r] > v
+        int i = l+1;    // arr[lt+1...i) == v
+        while( i < gt ){
+            if( arr[i].compareTo(v) < 0 ){
+                swap( arr, i, lt+1);
+                i ++;
+                lt ++;
+            }
+            else if( arr[i].compareTo(v) > 0 ){
+                swap( arr, i, gt-1);
+                gt --;
+            }
+            else{ // arr[i] == v
+                i ++;
+            }
+        }
+
+        swap( arr, l, lt );
+
+        sort(arr, l, lt-1);
+        sort(arr, gt, r);
     }
 
     public static void sort(Comparable[] arr){
@@ -56,4 +48,11 @@ public class MergeSortO{
         int n = arr.length;
         sort(arr, 0, n-1);
     }
+
+    private static void swap(Object[] arr, int i, int j) {
+        Object t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+    }
+
 }
